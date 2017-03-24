b0b3c9c9051846bad7e10eb3f9a283f59dc9c03a
Sothish Sasidharan <sothish@gmail.com>
2017-03-23 23:12:33 -0400

    iLoveMyCity

---
+++ app.js
@@ -0,0 +1,35 @@
+var express=require('express');
+var app=express();
+var path = require('path');
+
+var index = require('./routes/index');
+var cities = require('./routes/city');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', index.home);
+app.get('/:city', cities.city);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,5 @@
+exports.home=function(req,res){
+  res.render('home',{title:'iLoveMyCity',
+                headline:'We believe that every city have something to say!'
+                });
+              }
